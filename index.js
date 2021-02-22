046c00105107ca90155f257f88ffcebba0667a9f
chqiu6 <chucky-qiu@lambdastudents.com>
2021-02-21 21:01:46 -0500

    completed interview questions, added process.env to port to see if itll deploy successfully on heroku, missing middleware

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ Go code!
 */
 const server = require("./api/server");
 
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.log(`Server is listening at port : ${port} `);
